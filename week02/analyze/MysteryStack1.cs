166d7d2abb7f99d79b4b0b17285a7cab0e0c17aa
Nathaniel Dunham <dun19007@byui.edu>
2024-03-09 14:26:40 -0700

    did the analyze activity.

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -2,12 +2,16 @@
     public static string Run(string text) {
         var stack = new Stack<char>();
         foreach (var letter in text)
-            stack.Push(letter);
+            stack.Push(letter); //fill the stack with the characters of the string
 
         var result = "";
         while (stack.Count > 0)
-            result += stack.Pop();
+            result += stack.Pop(); // fill the result variable with the stack of characters
 
-        return result;
+        return result; // return the result string.
     }
+    //Returns should be the same as the inputs.
+    // racecar: racecar
+    // stressed: stressed
+    // 
 }
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,26 +1,26 @@
 ﻿public static class MysteryStack2 {
     private static bool IsFloat(string text) {
-        return float.TryParse(text, out _);
+        return float.TryParse(text, out _); //parse string as float.
     }
 
     public static float Run(string text) {
         var stack = new Stack<float>();
-        foreach (var item in text.Split(' ')) {
-            if (item == "+" || item == "-" || item == "*" || item == "/") {
-                if (stack.Count < 2)
+        foreach (var item in text.Split(' ')) { //traverse array of the words in the string.
+            if (item == "+" || item == "-" || item == "*" || item == "/") { //determine if the item is a arithmetic operator.
+                if (stack.Count < 2) //If there are only two items in the stack, throw an error.
                     throw new ApplicationException("Invalid Case 1!");
 
-                var op2 = stack.Pop();
-                var op1 = stack.Pop();
-                float res;
+                var op2 = stack.Pop(); // get the latest item
+                var op1 = stack.Pop(); // then get the item before it.
+                float res; //result variable
                 if (item == "+") {
-                    res = op1 + op2;
+                    res = op1 + op2; //add the two items
                 }
                 else if (item == "-") {
-                    res = op1 - op2;
+                    res = op1 - op2; //subtract the two items
                 }
                 else if (item == "*") {
-                    res = op1 * op2;
+                    res = op1 * op2; // multiply them.
                 }
                 else {
                     if (op2 == 0)
@@ -29,21 +29,27 @@
                     res = op1 / op2;
                 }
 
-                stack.Push(res);
+                stack.Push(res); //push the result to the stack.
             }
             else if (IsFloat(item)) {
-                stack.Push(float.Parse(item));
+                stack.Push(float.Parse(item)); //add float to stack
             }
-            else if (item == "") {
+            else if (item == "") { //if its nothing do nothing
             }
             else {
                 throw new ApplicationException("Invalid Case 3!");
             }
         }
 
-        if (stack.Count != 1)
+        if (stack.Count != 1) // if the math could not be performed
             throw new ApplicationException("Invalid Case 4!");
 
-        return stack.Pop();
+        return stack.Pop(); //return the stack's single item.
     }
+    //5 3 7 + *: 50
+    //6 2 + 5 3 - /: 4
+    //1 + 2 -: invalid case 1
+    //1 0 /: invalid case 2
+    //5 l: invalid case 3
+    //1 1 1: invalid case 4
 }
